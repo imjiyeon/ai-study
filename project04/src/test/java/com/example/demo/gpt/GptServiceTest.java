65625f6065381d304653e34f96bb4d0b10f62024
imjiyeon <yoyt12@naver.com>
2025-05-07 15:47:01 +0900

    ddd

---
+++ project04/src/test/java/com/example/demo/gpt/GptServiceTest.java
@@ -0,0 +1,21 @@
+//package com.example.demo.gpt;
+//
+//import com.example.demo.gpt.dto.ChatResponse;
+//import com.example.demo.gpt.service.GptService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class GptServiceTest {
+//
+//    @Autowired
+//    GptService service;
+//
+//    @Test
+//    void API호출테스트(){
+//        String response = service.callGptApi();
+//        System.out.println(response);
+//    }
+//
+//}
