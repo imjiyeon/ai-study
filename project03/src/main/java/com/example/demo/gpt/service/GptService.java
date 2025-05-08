13d98e7191963ca189cc6b8c3c4cb2629b6312f0
imjiyeon <yoyt12@naver.com>
2025-05-08 18:27:07 +0900

    ddd

--- project03/src/main/java/com/example/demo/gpt/service/GptService.java
+++ project03/src/main/java/com/example/demo/gpt/service/GptService.java
@@ -30,7 +30,6 @@ public class GptService {
 
         // 메시지 구성
         JSONArray messages = new JSONArray();
-        JSONObject message = new JSONObject();
 
         /*
         * developer라는 역할은 없음
@@ -46,6 +45,7 @@ public class GptService {
         /*
          * system role을 사용하여 서비스에 역할을 부여
          *  */
+        JSONObject message = new JSONObject();
         message.put("role", "system");
         message.put("content", "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다.");
         messages.put(message);
@@ -54,21 +54,25 @@ public class GptService {
         * system에 적용한 챗GPT의 역할을 USER에 중복 적용하면 역할이 더 확실하게 부여됨
         * system에 입력한 내용을 복사해서 넣기
         * */
-        message.put("role", "user");
-        message.put("content", "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다.");
-        messages.put(message);
+        JSONObject message2 = new JSONObject();
+        message2.put("role", "user");
+        message2.put("content", "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다.");
+        messages.put(message2);
     
         /*
         * 이전대화 학습시키기
         * assistant: 챗GPT의 응답
         * 그리고 다시 질문하기
         * */
-        message.put("role", "assistant");
-        message.put("content", "안녕하세요! 저는 챗도지입니다. 운세, 사주, 타로 등 다양한 분야의 운명과 인생 길흉화복에 대한 풍부한 지식을 바탕으로, 여러분의 질문에 명확하고 진솔하게 답해드리겠습니다. 인생의 방향, 사랑, 직업, 금전, 건강 등 궁금하신 점이나 알고 싶은 미래에 대해서 편하게 질문해 주세요. 여러분의 길을 밝히는 길잡이가 되어 드리겠습니다! 무엇이 궁금하신가요?");
-
-        message.put("role", "user");
-        message.put("content", "오늘의 운세가 뭐야?");
-        messages.put(message);
+        JSONObject message3 = new JSONObject();
+        message3.put("role", "assistant");
+        message3.put("content", "안녕하세요! 저는 챗도지입니다. 운세, 사주, 타로 등 다양한 분야의 운명과 인생 길흉화복에 대한 풍부한 지식을 바탕으로, 여러분의 질문에 명확하고 진솔하게 답해드리겠습니다. 인생의 방향, 사랑, 직업, 금전, 건강 등 궁금하신 점이나 알고 싶은 미래에 대해서 편하게 질문해 주세요. 여러분의 길을 밝히는 길잡이가 되어 드리겠습니다! 무엇이 궁금하신가요?");
+        messages.put(message3);
+        
+        JSONObject message4 = new JSONObject();
+        message4.put("role", "user");
+        message4.put("content", "오늘의 운세가 뭐야?");
+        messages.put(message4);
 
         // 요청 본문
         JSONObject json = new JSONObject();
